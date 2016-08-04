580fb0b6ab2d1ce1b3933ff35c1dc25905044d01
Hvorostenko Nikita <hvorostenko.nikita@gmail.com>
2016-08-04 10:54:28 +0300

    diy

--- silta_diy_module/classes/general/diy_element/shop_contacts.php
+++ silta_diy_module/classes/general/diy_element/shop_contacts.php
@@ -3,6 +3,7 @@ class SDiyModuleElementShopContacts extends SDiyModuleElement
 	{
 	protected function AccessCalculating()
 		{
+		if(CUser::IsAdmin()) return;
 		$DiyModule = SDiyModule::GetInstance();
 		foreach($this->GetPropertyList() as $property => $propertyObject)
 			if(!in_array($property, $DiyModule->GetModuleOption("shop_contacts_props_to_change")))
--- silta_diy_module/classes/general/diy_element/shops.php
+++ silta_diy_module/classes/general/diy_element/shops.php
@@ -3,6 +3,7 @@ class SDiyModuleElementShops extends SDiyModuleElement
 	{
 	protected function AccessCalculating()
 		{
+		if(CUser::IsAdmin()) return;
 		$DiyModule = SDiyModule::GetInstance();
 		foreach($this->GetPropertyList() as $property => $propertyObject)
 			if(!in_array($property, $DiyModule->GetModuleOption("shops_props_to_change")))
