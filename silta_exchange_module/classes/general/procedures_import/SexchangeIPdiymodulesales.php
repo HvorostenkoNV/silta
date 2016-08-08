370034c18d0cad696046e81888780ff06c44a21d
Hvorostenko Nikita <hvorostenko.nikita@gmail.com>
2016-08-08 10:16:32 +0300

    work

---
+++ silta_exchange_module/classes/general/procedures_import/SexchangeIPdiymodulesales.php
@@ -0,0 +1,14 @@
+
+<?
+final class SexchangeIPdiymodulesales extends SexchangeImportProcedure
+{
+protected $procedureName = "diy_module-sales";
+/* -------------------------------------------------------------------- */
+/* ----------- преобразовать полученные параметры процедуры ----------- */
+/* -------------------------------------------------------------------- */
+protected function ConvertParams(array $valueArray = [])
+{
+return $valueArray;
+}
+}
+?>
